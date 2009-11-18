21519963e52069be321df1c64427e2778d345bff
Chris Han <xiphux@gmail.com>
2009-11-17 22:36:03 -0600

    Add unicode methods

--- characters/models.py
+++ characters/models.py
@@ -3,11 +3,20 @@ from django.db import models
 class Character(models.Model):
 	name = models.CharField(max_length=200)
 
+	def __unicode__(self):
+		return self.name
+
 class Revision(models.Model):
 	character = models.ForeignKey(Character)
 	rev_date = models.DateTimeField('revision date')
+
+	def __unicode__(self):
+		return self.rev_date
 
 class Attribute(models.Model):
 	revision = models.ForeignKey(Revision)
 	name = models.CharField(max_length=200)
 	value = models.TextField()
+
+	def __unicode__(self):
+		return self.name
