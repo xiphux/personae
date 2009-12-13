779d38eadcc50495fe8d906617a5c9091ad2c02b
Chris Han <xiphux@gmail.com>
2009-12-12 22:52:13 -0600

    Use dimmed dots to show a pending change

--- media/characters/js/basesheet.js
+++ media/characters/js/basesheet.js
@@ -85,12 +85,14 @@ function setupDotWidgetEvents(type) {
   dotwidgets.live("mouseout", function(e) {
     var emptyclass = 'dotempty';
     var fullclass = 'dotfull';
+    var pendingclass = 'dotpending';
     var emptycontent = '&#9678;';
     var fullcontent = '&#9673;';
     var dotgroup = $(e.target).parent();
     if (dotgroup.hasClass('squarewidget')) {
       emptyclass = 'squareempty';
       fullclass = 'squarefull';
+      pendingclass = 'squarepending';
       emptycontent = '&#9634;';
       fullcontent = '&#9635;';
     }
@@ -109,37 +111,110 @@ function setupDotWidgetEvents(type) {
         }
       }
     }
+    dots.removeClass(pendingclass);
   });
   dotwidgets.live("click", function(e) {
     var dotgroup = $(e.target).parent()
+ 
+    var emptyclass = 'dotempty';
+    var fullclass = 'dotfull';
+    var pendingclass = 'dotpending';
+    var emptycontent = '&#9678;';
+    var fullcontent = '&#9673;';
+    if (dotgroup.hasClass('squarewidget')) {
+      emptyclass = 'squareempty';
+      fullclass = 'squarefull';
+      pendingclass = 'squarepending';
+      emptycontent = '&#9634;';
+      fullcontent = '&#9635;';
+    }
+
     var valelem = dotgroup.children('input');
     var newval = $(e.target).prevAll('span').size() + 1;
     var currentval = (valelem.val() * 1);
     if ((newval == 1) && (currentval == 1)) {
-      valelem.val(0);
-    } else {
-      valelem.val(newval);
+      newval = 0;
     }
-    dotgroup.highlightFade({
-      speed:200
-    });
+    valelem.val(newval);
+    var dots = dotgroup.children('span');
+    var count = dots.size();
+    dots.removeClass(pendingclass);
+    for (var idx = 0; idx < count; idx++) {
+      var curdot = dots.eq(idx);
+      if ((idx < newval) || ((idx == 0) && (newval == 0))) {
+        if (curdot.hasClass(emptyclass)) {
+          curdot.html(fullcontent).removeClass(emptyclass).addClass(fullclass);
+        }
+      } else {
+        if (curdot.hasClass(fullclass)) {
+          curdot.html(emptycontent).removeClass(fullclass).addClass(emptyclass);
+        }
+      }
+      if ((idx == 0) && (newval < 2)) {
+        curdot.addClass(pendingclass);
+      }
+    }
   });
   $("span." + widgetclass + " > span").live("mouseover", function(e) {
     var emptyclass = 'dotempty';
     var fullclass = 'dotfull';
+    var pendingclass = 'dotpending';
     var emptycontent = '&#9678;';
     var fullcontent = '&#9673;';
     var dot = $(e.target);
     if (dot.parent().hasClass('squarewidget')) {
       emptyclass = 'squareempty';
       fullclass = 'squarefull';
+      pendingclass = 'squarepending';
       emptycontent = '&#9634;';
       fullcontent = '&#9635;';
     }
-    if (dot.hasClass(emptyclass)) {
-      dot.html(fullcontent).removeClass(emptyclass).addClass(fullclass);
+
+    var currentval = (dot.siblings('input').val() * 1);
+    var currentdot = dot.prevAll('span').size() + 1;
+    var dotgroup = dot.parent();
+    var dots = dotgroup.children('span');
+    var count = dots.size();
+
+    var solidval = 0;
+    var pendingval = 0;
+    if (currentval < currentdot) {
+      solidval = currentval;
+      pendingval = currentdot;
+    } else {
+      solidval = currentdot;
+      pendingval = currentval;
     }
-    dot.prevAll('span.' + emptyclass).html(fullcontent).removeClass(emptyclass).addClass(fullclass);
-    dot.nextAll('span.' + fullclass).html(emptycontent).removeClass(fullclass).addClass(emptyclass);
+
+    for (var idx = 0; idx < count; idx++) {
+      var idxdot = dots.eq(idx);
+      if (idx < pendingval) {
+        if (idxdot.hasClass(emptyclass)) {
+          idxdot.html(fullcontent).removeClass(emptyclass).addClass(fullclass);
+        }
+      } else {
+        if (idxdot.hasClass(fullclass)) {
+          idxdot.html(emptycontent).removeClass(fullclass).addClass(emptyclass);
+        }
+      }
+
+      if ((currentval == 1) && (currentdot == 1) && (idx == 0)) {
+        if (!(idxdot.hasClass(pendingclass))) {
+          idxdot.addClass(pendingclass);
+        }
+      } else if (idx < solidval) {
+        if (idxdot.hasClass(pendingclass)) {
+          idxdot.removeClass(pendingclass);
+        }
+      } else if (idx < pendingval) {
+        if (!(idxdot.hasClass(pendingclass))) {
+          idxdot.addClass(pendingclass);
+        }
+      } else {
+        if (idxdot.hasClass(pendingclass)) {
+          idxdot.removeClass(pendingclass);
+        }
+      }
+    }
   });
 }
