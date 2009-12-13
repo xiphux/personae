264fbfc6890d29b4da9b98d855d3ace14e3402d5
Chris Han <xiphux@gmail.com>
2009-12-13 01:56:31 -0600

    Clean up dynamic row javascript

--- media/characters/js/universe/scion.js
+++ media/characters/js/universe/scion.js
@@ -1,132 +1,51 @@
-function addBirthright(animate) {
-  var id = document.getElementById("birthrightid").value;
-  id = (id * 1) + 1;
-  var field = $('#birthright0').clone();
-  field.attr('id','birthright' + id);
+function addFieldRow(srcelem, container, animate) {
+  var field = $(srcelem).clone();
+  field.attr('id','');
   field.show();
-  field.appendTo("#birthrightsrows");
+  field.appendTo(container);
   if (animate) {
-    $("#birthright" + id).highlightFade({
-      speed:1000
-    });
-  }
-  document.getElementById("birthrightid").value = id.toString();
-  setupDotWidgets("dot");
-}
-
-function addWeapon(animate) {
-  var id = document.getElementById("weaponid").value;
-  id = (id * 1) + 1;
-  var field = $('#weapon0').clone();
-  field.attr('id','weapon' + id);
-  field.show();
-  field.appendTo('#weapontablerows');
-  if (animate) {
-    $("#weapon" + id).highlightFade({
+    field.highlightFade({
       speed:1000
     })
   }
-  document.getElementById("weaponid").value = id.toString();
 }
 
-function addVirtue(animate) {
-  var id = document.getElementById("virtueid").value;
-  id = (id * 1) + 1;
-  var field = $('#virtue0').clone();
-  field.attr('id','virtue' + id);
-  field.show();
-  field.appendTo('#virtuerows');
-  if (animate) {
-    $("#virtue" + id).highlightFade({
-      speed:1000
-    })
-  }
-  document.getElementById("virtueid").value = id.toString();
-}
-
-function addBoon(animate) {
-  var id = document.getElementById("boonid").value;
-  id = (id * 1) + 1;
-  var field = $('#boon0').clone();
-  field.attr('id','boon' + id);
-  field.show();
-  field.appendTo('#boonrows');
-  if (animate) {
-    $("#boon" + id).highlightFade({
-      speed:1000
-    })
-  }
-  document.getElementById("boonid").value = id.toString();
-}
-
-function addRelic(animate) {
-  var id = document.getElementById("relicid").value;
-  id = (id * 1) + 1;
-  var field = $('#relic0').clone();
-  field.attr('id','relic' + id);
-  field.show();
-  field.appendTo('#relicrows');
-  if (animate) {
-    $("#relic" + id).highlightFade({
-      speed:1000
-    })
-  }
-  document.getElementById("relicid").value = id.toString();
-}
-
-function addKnack(animate) {
-  var id = document.getElementById("knackid").value;
-  id = (id * 1) + 1;
-  var field = $('#knack0').clone();
-  field.attr('id','knack' + id);
-  field.show();
+function moveKnackClear() {
   var knackclear = $('#knackclear').clone();
   $('#knackclear').remove();
-  field.appendTo('#knackrows');
   knackclear.appendTo('#knackrows');
-  if (animate) {
-    $("#knack" + id).highlightFade({
-      speed:1000
-    })
-  }
-  document.getElementById("knackid").value = id.toString();
 }
 
 function setupDynamicTables() {
   $('#birthright0').hide();
-  if (document.getElementById("birthrightid").value == 0) {
-    addBirthright(false);
-    //$("#birthright1 > .removerow").hide();
+  if ($('#birthrightsrows > tr').size() == 0) {
+    addFieldRow('#birthright0','#birthrightsrows',false);
   }
 
   $('#weapon0').hide();
-  if (document.getElementById("weaponid").value == 0) {
-    addWeapon(false);
-    //$("#weapon1 > .removerow").hide();
+  if ($('#weapontablerows > tr').size() == 0) {
+    addFieldRow('#weapon0','#weapontablerows',false);
   }
 
   $('#virtue0').hide();
-  if (document.getElementById("virtueid").value == 0) {
-    addVirtue(false);
-    //$("#virtue1 > .removerow").hide();
+  if ($('#virtuerows > tr').size() == 0) {
+    addFieldRow('#virtue0','#virtuerows',false);
   }
 
   $('#boon0').hide();
-  if (document.getElementById("boonid").value == 0) {
-    addBoon(false);
-    //$("#boon1 > .removerow").hide();
+  if ($('#boonrows > tr').size() == 0) {
+    addFieldRow('#boon0','#boonrows',false);
   }
 
   $('#relic0').hide();
-  if (document.getElementById("relicid").value == 0) {
-    addRelic(false);
-    //$("#relic1 > .removerow").hide();
+  if ($('#relicrows > tr').size() == 0) {
+    addFieldRow('#relic0','#relicrows',false);
   }
 
   $('#knack0').hide();
-  if (document.getElementById("knackid").value == 0) {
-    addKnack(false);
-    //$("#knack1 > .removerow").hide();
+  if ($('#knackrows > div.knackitem').size() <= 1) {
+    addFieldRow('#knack0','#knackrows',false);
+    moveKnackClear();
   }
 
 }
